573935227dadf4cf82aa185a90f7308289f6e198
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-04 12:32:07 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Telegram Mini App</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
     {{-- <script src="https://telegram.org/js/telegram-web-app.js"></script> --}}
 
@@ -13,10 +14,10 @@
             window.Telegram.WebApp.disableBackgroundOverride();
         }
     </script>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    {{-- @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
 
 </head>
-<body class="bg-gray-100" id="app" >
+<body class="bg-gray-100" id="app">
     <div class="text-sky-500 text-2xl">
         {{-- {{ $user['first_name'] }} --}}
         <div class="p-4 rounded-lg bg-gray-100 shadow-md">
