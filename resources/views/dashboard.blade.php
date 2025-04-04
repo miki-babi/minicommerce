7e5a22c9f9b85acd44d642945bd7395ecee03616
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-04 11:19:40 +0300

    test

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+    {{ $user->first_name }} {{ $user->last_name }}
+    <br>
+    {{ $user->username }}
+    <br>
+    {{ $user->id }}
+</div>
