4d0ec0efdf0b7c1807006770e622fbea68e44910
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-04 15:19:39 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,39 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Telegram Mini App</title>
-
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
-
-    
     @vite(['resources/js/app.js', 'resources/css/app.css'])
-
 </head>
 <body class="bg-red-100" id="app">
     <div class="text-sky-500 text-2xl">
-        {{-- {{ $user['first_name'] }} --}}
         <div class="p-4 rounded-lg bg-gray-100 shadow-md">
             heello
-            </div>
+        </div>
     </div>
 
-    
     <script>
-    
-    
         document.addEventListener("DOMContentLoaded", function() {
             const tg = window.Telegram.WebApp;
             tg.expand();
-    
+
             if (tg.initData.user) {
                 const userId = tg.initData.user.id;
                 const firstName = encodeURIComponent(tg.initData.user.first_name);
                 const lastName = encodeURIComponent(tg.initData.user.last_name || '');
                 const username = encodeURIComponent(tg.initData.user.username || '');
-                // const photourl = encodeURIComponent(tg.initDataUnsafe.user.username || '');
-                const photoUrl = encodeURIComponent(tg.initDataUnsafe.user.photo_url || '');
-    
-                // Use Laravel's route name (manually constructed since JS can't call route() directly)
+                const photoUrl = encodeURIComponent(tg.initData.user.photo_url || '');
+
+                // Correct usage of Blade route
                 const routeName = "{{ route('dashboard') }}";
-                window.location.href = `${routeName}?user_id=${userId}&first_name=${firstName}&last_name=${lastName}&username=${username}&photo_url=${photoUrl}`;
+                const url = `${routeName}?user_id=${userId}&first_name=${firstName}&last_name=${lastName}&username=${username}&photo_url=${photoUrl}`;
+                
+                window.location.href = url;
             }
         });
-    
-        
-    
-    
-        </script>
+    </script>
 </body>
 </html>
