977c4e37cb99c546d951f1b6ad8a67d2d2224f55
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-04 15:38:10 +0300

    text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,12 +8,20 @@
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body class="bg-white" id="app">
-    <div class="text-sky-500 text-2xl font-bold p-4 rounded-lg bg-gray-100 shadow-md">
-        <p>
-            Hello, {{ $user['first_name'] }}! Welcome to the Telegram Mini App.
-        </p>
-        <p>{{ $user['photo_url'] }}</p>
-        <img src="{{ $user['photo_url'] }}" alt="User Photo" class="rounded-full w-16 h-16 mt-2">
+    <div class="text-sky-500 text-2xl font-bold p-4 rounded-lg bg-gray-100 shadow-md flex items-center justify-between">
+        <div class="text-black" >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              
+        </div>
+        <div>
+            <p>
+                {{ $user['first_name'] }}
+            </p> 
+            <img src="{{ $user['photo_url'] }}" alt="User Photo" class="rounded-full w-16 h-16 mt-2">
+        </div>
+        
         {{-- {{ $user['first_name'] }} --}}
     </div>
 
