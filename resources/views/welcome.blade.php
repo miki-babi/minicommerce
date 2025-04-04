8fdf65252ae909e851482afd88d41c7fd5c98577
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-04 15:33:37 +0300

    testxy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,44 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Telegram Mini App</title>
+
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
+
+    
     @vite(['resources/js/app.js', 'resources/css/app.css'])
+
 </head>
 <body class="bg-red-100" id="app">
     <div class="text-sky-500 text-2xl">
+        {{-- {{ $user['first_name'] }} --}}
         <div class="p-4 rounded-lg bg-gray-100 shadow-md">
             heello
-        </div>
+            </div>
     </div>
 
+    
     <script>
+    
+    
         document.addEventListener("DOMContentLoaded", function() {
             const tg = window.Telegram.WebApp;
-
-            // alert(tg.initData);
             tg.expand();
-
+    
             if (tg.initDataUnsafe.user) {
-                const userId = tg.initData.user.id;
+                const userId = tg.initDataUnsafe.user.id;
                 const firstName = encodeURIComponent(tg.initDataUnsafe.user.first_name);
                 const lastName = encodeURIComponent(tg.initDataUnsafe.user.last_name || '');
                 const username = encodeURIComponent(tg.initDataUnsafe.user.username || '');
+                // const photourl = encodeURIComponent(tg.initDataUnsafe.user.username || '');
                 const photoUrl = encodeURIComponent(tg.initDataUnsafe.user.photo_url || '');
-
-                // Alert the values to verify
-                alert('User ID: ' + userId);
-                alert('First Name: ' + firstName);
-                alert('Last Name: ' + lastName);
-                alert('Username: ' + username);
-                alert('Photo URL: ' + photoUrl);
-
-                // Correct usage of Blade route
+    
+                // Use Laravel's route name (manually constructed since JS can't call route() directly)
                 const routeName = "{{ route('dashboard') }}";
-                const url = `${routeName}?user_id=${userId}&first_name=${firstName}&last_name=${lastName}&username=${username}&photo_url=${photoUrl}`;
-                
-                window.location.href = url;
+                window.location.href = `${routeName}?user_id=${userId}&first_name=${firstName}&last_name=${lastName}&username=${username}&photo_url=${photoUrl}`;
             }
         });
-    </script>
+    
+        
+    
+    
+        </script>
 </body>
 </html>
