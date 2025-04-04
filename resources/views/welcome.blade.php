8b215661f0256ff5196d56d68633bf5fd12a52e7
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-04 11:59:41 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Telegram Mini App</title>
-    <script src="https://telegram.org/js/telegram-web-app.js">tg.disableBackgroundOverride();</script>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
+
+    <script src="https://telegram.org/js/telegram-web-app.js">tg.disableBackgroundOverride();</script>
 </head>
 <body class="bg-gray-100" id="app">
     <div class="text-sky-500 text-2xl">
