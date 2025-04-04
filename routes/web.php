f542717e25d180b58439a8d0134fa5eb4f80a36b
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-04 11:06:06 +0300

    sync with prev

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,30 @@
 <?php
 
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function (Request $request) {
- 
+
+Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function (Request $request) {
+    // dd($request->all());
+    
+    $user = [
+        'id' => $request->input('user_id'),
+        'first_name' => $request->input('first_name'),
+        'last_name' => $request->input('last_name'),
+        'username' => $request->input('username'),
+    ];
+dd($user);
+    return view('dashboard', compact('user'));
+})->name('dashboard');
+
+
+// Route::get('/dashboard', function (Request $request) {
+//     // Decode the Telegram data from the query string
+//     $telegramData = json_decode($request->input('telegram_data'), true);
+//     $data=$telegramData['user']['first_name'];
+// dd( $data);
+//     return view('dashboard', compact('telegramData'));
+// })->name('dashboard');
