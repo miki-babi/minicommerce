868b10069e5d03eb5bef5487aab908ddc14987ed
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-07 10:37:03 +0300

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
 }" class="bg-white" id="app">
     <div class="text-sky-500 text-2xl font-bold p-4 rounded-lg bg-gray-100 shadow-md flex items-center justify-between">
         <div class="text-black"  @click="open = !open">
-            <div x-show="!open">
+            <div x-show="!open" class="p-2 hover:bg-gray-200  border-2 rounded-md cursor-pointer">
                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
@@ -31,14 +31,7 @@
         </div>
     </div>
     <div  x-show="open"  class ="absolute top-16 left-4 bg-white shadow-lg rounded-lg p-4 z-10">
-        <div class="flex justify-end items-center mb-4">
-            <div class="p-2 hover:bg-gray-200  border-2 rounded-md cursor-pointer" @click="open = !open">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                  </svg>   
-            </div>
-                      
-        </div>
+        
         <ul>
             <li class="p-2 hover:bg-gray-200 cursor-pointer" @click="open = !open">
                 <a href="#">Dashboard</a>
